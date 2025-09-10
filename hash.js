929de3d4ac3f38feae542f9353446e7877b519ce
Jasenovec <rg_montano@hotmail.com>
2025-09-10 00:31:00 -0500

    hash

--- hash.js
+++ hash.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 
 async function generarHash() {
-  const password = "auxmañana123"; // aquí pones la contraseña que quieres
+  const password = "admin"; // aquí pones la contraseña que quieres
   const hash = await bcrypt.hash(password, 10);
   console.log("Hash generado:", hash);
 }
